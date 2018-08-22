750617b29641d70e5e2f99107d151b73deb695d8
Gustavo Diel <gustavodiel@hotmail.com>
2018-08-22 08:12:57 -0300

    Added Python Bubble Sort

--- python/bubble_sort/bubble_sort.py
+++ python/bubble_sort/bubble_sort.py
@@ -1,1 +1,48 @@
+#! /usr/bin/python
 
+from time import time
+import random
+
+
+def bubble_sort(array):
+    for i in array:
+        for j in array:
+            if i > j:
+                tmp = i
+                i = j
+                j = tmp
+
+
+def generate_ordered_array(number):
+    return [i for i in range(number)]
+
+
+def generate_reversed_array(number):
+    return [number - i for i in range(number)]
+
+
+def generate_random_array(number):
+    return [random.randint(0, number) for i in range(number)]
+
+
+if __name__ == '__main__':
+    numbers_to_order = 5000
+
+    ordered_array = generate_ordered_array(numbers_to_order)
+    reversed_array = generate_reversed_array(numbers_to_order)
+    random_array = generate_random_array(numbers_to_order)
+
+    start = time()
+    bubble_sort(ordered_array)
+    finish = time()
+    print("Time to execute ORDERED: {}".format(finish - start))
+
+    start = time()
+    bubble_sort(reversed_array)
+    finish = time()
+    print("Time to execute REVERSED: {}".format(finish - start))
+
+    start = time()
+    bubble_sort(random_array)
+    finish = time()
+    print("Time to execute RANDOM: {}".format(finish - start))
